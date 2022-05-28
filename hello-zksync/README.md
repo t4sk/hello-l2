2fbf1657020c3669c4cf9b90a98e5a76b39ed249
t4sk <tsk.nakamura@gmail.com>
2022-05-28 13:32:21 +0900

    zksync

---
+++ hello-zksync/README.md
@@ -0,0 +1,38 @@
+### Install
+
+```shell
+npm i
+```
+
+### Compile
+
+```shell
+# 1. Create .secret and paste your private key
+
+# 2. Complie contract. Make sure Docker is enabled before running the command below
+npx hardhat compile
+```
+
+### Deploy
+
+```
+npx hardhat deploy-zksync
+```
+
+### Deployed contract
+
+0x26C4ca34f722BD8fD23D58f34576d8718c883A80
+
+https://zksync2-testnet.zkscan.io/address/0x26C4ca34f722BD8fD23D58f34576d8718c883A80
+
+### Bridge
+
+https://portal.zksync.io/bridge
+
+### Metamask Testnet Network Info
+
+https://v2-docs.zksync.io/dev/testnet/metamask.html
+
+### zkSync 2.0 Tutorial
+
+https://v2-docs.zksync.io/dev/guide/hello-world.html#prerequisites
---
+++ hello-zksync/deploy/deploy.ts
@@ -0,0 +1,38 @@
+import fs from "fs";
+import { utils, Wallet } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+const PRIV_KEY = fs.readFileSync(".secret").toString();
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the Greeter contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet(PRIV_KEY);
+
+  // Create deployer object and load the artifact of the contract we want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("Greeter");
+
+  // // Deposit some funds to L2 in order to be able to perform L2 transactions.
+  // const depositAmount = ethers.utils.parseEther("0.001");
+  // const depositHandle = await deployer.zkWallet.deposit({
+  //   to: deployer.zkWallet.address,
+  //   token: utils.ETH_ADDRESS,
+  //   amount: depositAmount,
+  // });
+  // // Wait until the deposit is processed on zkSync
+  // await depositHandle.wait();
+
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const greeting = "Hi there!";
+  const greeterContract = await deployer.deploy(artifact, [greeting]);
+
+  // Show the contract info.
+  const contractAddress = greeterContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+}
